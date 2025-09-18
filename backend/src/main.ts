cc01c3e7b6cd84588927902c5bab85cba0888e5e
Suresh Dube <>
2025-09-18 20:22:07 +0530

    temporary fix for cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,8 +11,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
+      '*',
       'http://localhost',
-       'http://localhost:3000',
+      'http://localhost:3000',
       'http://localhost:5173',
       'http://127.0.0.1',
       'http://127.0.0.1:5173',
